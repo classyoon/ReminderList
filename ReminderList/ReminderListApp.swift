58b073d9c3c8a3329a5572dc1369e619dfabf2e1
Conner Yoon <con.yoon@icloud.com>
2022-08-06 09:34:50 -0400

    Initial Commit

---
+++ ReminderList/ReminderListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderListApp.swift
+//  ReminderList
+//
+//  Created by Conner Yoon on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ReminderListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
